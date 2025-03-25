0e9fce12759d522af65054f6366d590274398a5c
Alim Sourav <sourav.0.alim@gmail.com>
2025-03-26 01:26:37 +0600

    Added Publishing Details

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 kotlin {
@@ -55,4 +57,42 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.alims-repo",
+        artifactId = "prayer-times-kmm",
+        version = "1.0.1-beta"
+    )
+
+    pom {
+        name.set("Prayer Times KMM")
+        description.set("A lightweight and accurate Kotlin Multiplatform library for calculating Islamic prayer times, inspired by the Adhan algorithm and optimized for Android, iOS, JVM, and beyond.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Alims-Repo/Prayer-Times-KMM")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("alim")
+                name.set("Alim Sourav")
+                email.set("sourav.0.alim@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Alims-Repo/Prayer-Times-KMM")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
